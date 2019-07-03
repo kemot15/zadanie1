a5e6793b8f84add022da776778a65a40e4fdc5fd
Tomek <kemot15@tlen.pl>
2019-07-03 16:50:59 +0200

    pierwsza edycja

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -1,5 +1,6 @@
 public class Zadanie1 {
     public static void main(String[] args) {
         System.out.println("Ucze sie programowania i commitowania");
+        System.out.println("pierwsza edycja");
     }
 }

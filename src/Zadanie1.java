56e258b9f57dca05c432327e0875750bf90a837b
Tomek <kemot15@tlen.pl>
2019-07-03 16:47:59 +0200

    + commitowania

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -1,5 +1,5 @@
 public class Zadanie1 {
     public static void main(String[] args) {
-        System.out.println("Ucze sie programowania");
+        System.out.println("Ucze sie programowania i commitowania");
     }
 }
